5c268447d95cc8cc04dc25da0af5919503573768
40detectives <9024463+40detectives@users.noreply.github.com>
2023-06-21 14:22:59 +0200

    chore(tooling): set build directory in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src", // needed to be set here because index.html is not at the root dir, but in "src" dir
+  build: {
+    outDir: "../dist", // if not, will try to output to the folderd: "<current dir>/dist"
+  },
 });
