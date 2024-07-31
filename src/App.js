d1470ad64fe5680dec038db4ece9cd0e69308dea
40detectives <9024463+40detectives@users.noreply.github.com>
2024-07-31 21:45:07 +0200

    example of one-way data flow with props in components w/o jsx

---
+++ src/App.js
@@ -0,0 +1,26 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ])
+}
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {
+      /* here goes any attribute for the element like "id", etc*/
+    },
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {animal: "Dog", name: "Luna", breed: "Havanese"}),
+      React.createElement(Pet, {animal: "Bird", name: "Pepper", breed: "Cockatiel"}),
+      React.createElement(Pet, {animal: "Cat", name: "Doink", breed: "Mixed"}),
+    ]
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
