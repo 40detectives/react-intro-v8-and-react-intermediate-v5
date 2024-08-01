0eb28421ef81d2659b301f6d028eeb50f6dd6a4f
40detectives <9024463+40detectives@users.noreply.github.com>
2024-08-01 15:04:57 +0200

    (tooling): add React imports to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
     React.createElement("h2", {}, props.breed),
   ]);
-}
+};
 
 const App = () => {
   return React.createElement(
@@ -14,10 +17,10 @@ const App = () => {
     },
     [
       React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {animal: "Dog", name: "Luna", breed: "Havanese"}),
-      React.createElement(Pet, {animal: "Bird", name: "Pepper", breed: "Cockatiel"}),
-      React.createElement(Pet, {animal: "Cat", name: "Doink", breed: "Mixed"}),
-    ]
+      React.createElement(Pet, { animal: "Dog", name: "Luna", breed: "Havanese" }),
+      React.createElement(Pet, { animal: "Bird", name: "Pepper", breed: "Cockatiel" }),
+      React.createElement(Pet, { animal: "Cat", name: "Doink", breed: "Mixed" }),
+    ],
   );
 };
 
