7627b5b5cbb95f9bba9c418788b064df000f75ee
40detectives <9024463+40detectives@users.noreply.github.com>
2024-08-01 14:32:15 +0200

    (tooling): [optional] config build directory for vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   root: "src", // needed to be set here because index.html is not at the root dir, but in "src" dir
+  build: {
+    outDir: "../dist", // if not, will try to output to the folderd: "<current dir>/dist"
+  },
 });
