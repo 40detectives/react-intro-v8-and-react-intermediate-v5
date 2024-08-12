34fcd89595ea6797e691e02fae95b2fc356974fb
40detectives <9024463+40detectives@users.noreply.github.com>
2024-08-12 15:57:44 +0200

    (refactor): replace React.createElement with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {
-      /* here goes any attribute for the element like "id", etc*/
-    },
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, { animal: "Dog", name: "Luna", breed: "Havanese" }),
-      React.createElement(Pet, { animal: "Bird", name: "Pepper", breed: "Cockatiel" }),
-      React.createElement(Pet, { animal: "Cat", name: "Doink", breed: "Mixed" }),
-    ],
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doik" animal="Cat" breed="Mixed" />
+    </div>
   );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const root = createRoot(container);
+root.render(<App />);
