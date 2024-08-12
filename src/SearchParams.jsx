e15e9ed36e28139bc898463d3bf815f8ee594c95
40detectives <9024463+40detectives@users.noreply.github.com>
2024-08-12 23:05:48 +0200

    (jsx): map through arrays to return React Components

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState(""); // the mock API works with strings, try: "Seattle, WA"
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -15,6 +20,36 @@ const SearchParams = () => {
             placeholder="Seattle, WA"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
