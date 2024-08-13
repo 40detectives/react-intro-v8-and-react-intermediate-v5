152eb6e24fcad0ee623787d30f4e8a34c3d3e9d9
40detectives <9024463+40detectives@users.noreply.github.com>
2024-08-13 18:02:21 +0200

    (react-query): use 'useQuery' to fetch pet details

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,9 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams();
+  const results = useQuery({ queryKey: ["details", id], queryFn: fetchPet });
 
-  return <h2>{id}</h2>;
+  if (results.isError) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">💀</h2>
+        <h2>Oh no! Something went wrong</h2>
+        <h2 className="loader">💀</h2>
+      </div>
+    );
+  }
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🐣</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} – {pet.breed} – {pet.city}, {pet.state}
+        </h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
