46c9b5dbb3f8b37b557792eeda960f0e0aa498f4
40detectives <9024463+40detectives@users.noreply.github.com>
2024-09-05 12:30:42 +0200

    (ssr): Separate client exclusive staff to (like BrowserRouter component)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { lazy, StrictMode, Suspense, useState } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { lazy, Suspense, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import AdoptedPetContext from "./AdoptedPetContext";
 
 const Details = lazy(() => import("./Details"));
@@ -20,34 +19,26 @@ const App = () => {
   const adoptedPet = useState(null);
 
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <Suspense
-          fallback={
-            <div className="loading-pane">
-              <h2 className="loader">🐶</h2>
-            </div>
-          }
-        >
-          <AdoptedPetContext.Provider value={adoptedPet}>
-            <header>
-              <Link to="/">Adopt Me!</Link>
-            </header>
-            <Routes>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
-            </Routes>
-          </AdoptedPetContext.Provider>
-        </Suspense>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <Suspense
+        fallback={
+          <div className="loading-pane">
+            <h2 className="loader">🐶</h2>
+          </div>
+        }
+      >
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
+      </Suspense>
+    </QueryClientProvider>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+export default App;
